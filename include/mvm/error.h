79c27671261747235513e5a2f30d2b9d37e2ddc4
Nykenik24 <Nykenik24@proton.me>
2026-02-27 21:03:08 +0100

    fix: errno

--- include/mvm/error.h
+++ include/mvm/error.h
@@ -1,23 +1,18 @@
+// mvm_error.h
 #ifndef _MVM_ERROR_H
 #define _MVM_ERROR_H
 
 typedef enum {
   MVM_NOERR,
   MVM_UNRECOGNIZED_OPERATION,
-  MVM_PCOFFSET_OUT_OF_BOUNDS,
+  MVM_OFFSET_OUT_OF_BOUNDS,
   MVM_REGISTER_OUT_OF_BOUNDS,
-  MVM_CODE_LEN_OUT_OF_BOUNDS
+  MVM_CODE_LEN_OUT_OF_BOUNDS,
+  MVM_OUT_OF_USEMEM_BOUNDS
 } MVM_ERRNO;
 
-static MVM_ERRNO mvm_errno = MVM_NOERR;
-
-static const char *mvm_errstr[] = {
-    [MVM_NOERR] = "Used 'errprint', but there was no error",
-    [MVM_UNRECOGNIZED_OPERATION] = "Operation not recognized",
-    [MVM_PCOFFSET_OUT_OF_BOUNDS] = "PCOffset out of bounds",
-    [MVM_REGISTER_OUT_OF_BOUNDS] = "Register out of bounds (max is 7)",
-    [MVM_CODE_LEN_OUT_OF_BOUNDS] =
-        "Loaded code surpasses the set code length limit."};
+extern MVM_ERRNO mvm_errno;
+extern const char *mvm_errstr[];
 
 void errprint(const char *fmt, ...);
 
--- src/error.c
+++ src/error.c
@@ -2,6 +2,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+MVM_ERRNO mvm_errno = MVM_NOERR;
+
+const char *mvm_errstr[] = {
+    [MVM_NOERR] = "Used 'errprint', but there was no error",
+    [MVM_UNRECOGNIZED_OPERATION] = "Operation not recognized",
+    [MVM_OFFSET_OUT_OF_BOUNDS] = "Offset out of bounds",
+    [MVM_REGISTER_OUT_OF_BOUNDS] = "Register out of bounds (max is 7)",
+    [MVM_CODE_LEN_OUT_OF_BOUNDS] =
+        "Loaded code surpasses the set code length limit",
+    [MVM_OUT_OF_USEMEM_BOUNDS] = "Index is out of usable memory bounds"};
+
 void errprint(const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
